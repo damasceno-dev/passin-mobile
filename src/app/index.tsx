e1b31acf54d4a8f34d075d39821e5500b10833d3
fernandodamasceno <damasceno.developer21@gmail.com>
2024-06-07 12:54:09 -0300

    feat: add native wind config files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import {Text, View} from "react-native";
 
 export default function Home() {
     return (
-        <View>
+        <View className="">
             <Text>Hello React Native!</Text>
         </View>
     )
