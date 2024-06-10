3610baa291376260cae9b2ff47d1627e3852832d
fernandodamasceno <damasceno.developer21@gmail.com>
2024-06-10 12:46:42 -0300

    feat: add credential interfaces

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,17 @@
 import "../styles/global.css"
 import {Slot} from "expo-router";
+import {Roboto_700Bold, Roboto_500Medium, Roboto_400Regular, useFonts} from "@expo-google-fonts/roboto"
+import {Loading} from "@/components/loading";
 
 
 export default function Layout() {
-    return (
-        <Slot/>        
-    )
+    const [fontsLoaded] = useFonts({
+        Roboto_700Bold,
+        Roboto_500Medium,
+        Roboto_400Regular,
+    });
+    if (!fontsLoaded) {
+        return <Loading/>
+    }
+    return <Slot/>
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,46 @@
-import {Text, View} from "react-native";
+import {Alert, Image, StatusBar, Text, View} from "react-native";
+import {Input} from "@/components/input";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {colors} from "@/styles/colors";
+import {Button} from "@/components/button";
+import {Link} from "expo-router";
+import {useState} from "react";
 
 export default function Home() {
+    
+    const [code, setCode] = useState("");
+    
+    function handleAccessCredentials() {
+        if (!code.trim()) {
+            return Alert.alert("Ingresso", "Informe o código do ingresso!")
+        }
+    }
+    
     return (
-        <View className="">
-            <Text>Hello React Native!</Text>
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <StatusBar barStyle="light-content"></StatusBar>
+            <Image 
+                source={require("@/assets/logo.png")}
+                className="h-16"
+                resizeMode="contain"
+            />
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <MaterialCommunityIcons 
+                        name="ticket-confirmation-outline"
+                        size={20}
+                        color={colors.green[200]}
+                    />
+                    <Input.Field
+                        placeholder="Código do ingresso"
+                        onChangeText={setCode}
+                    />
+                </Input>
+                <Button title="Acessar credencial" onPress={handleAccessCredentials}/>
+                <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8">
+                    Ainda não possui ingresso?
+                </Link>
+            </View>
         </View>
     )
 }
